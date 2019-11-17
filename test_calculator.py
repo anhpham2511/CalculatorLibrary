2e279c3e816ad4875193096fea30cfaa1413bf83
anhpham2511 <ms.phamanh@gmail.com>
2019-11-17 20:51:23 +0800

    Fix test 3

--- test_calculator.py
+++ test_calculator.py
@@ -4,8 +4,5 @@ class TestCalculator:
     def test_addition(self):
         assert 4 == calculator.add(2, 2)
         
-
     def test_subtraction(self):
         assert 2 == calculator.subtract(4, 2)
-
-            
